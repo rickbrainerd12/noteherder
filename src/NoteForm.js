78af655e37034cb674d968044c61d3dec95a3e7d
Rick Brainerd <rickbrainerd12@gmail.com>
2017-07-05 11:16:11 -0400

    Syle NoteLlist and NoteFOrm

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,8 +1,23 @@
 import React, {Component} from 'react'
+import './NoteForm.css'
 
 class NoteForm extends Component {
     render () {
-        return <p>Note Form </p>
+        return (
+        <div className="NoteForm">
+          <div className="form-actions">
+            <button type="button"><i className="fa fa-trash-o"></i>
+            </button>
+          </div>
+          <form>
+            <p>
+              <input type="text" name="title" placeholder="Title your note" value="Kohlrabi welsh"/>
+            </p>
+            
+            <textarea name="body"></textarea>
+          </form>
+        </div>
+        )
     }
 }
 
