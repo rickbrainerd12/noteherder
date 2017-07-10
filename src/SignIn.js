50c69ae4247cd2a0002c3f50bf2be7b1471182ca
Rick Brainerd <rickbrainerd12@gmail.com>
2017-07-10 11:21:06 -0400

    Style sign in page but somehow broke sign out button

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,19 +1,35 @@
 import React from 'react'
-import {auth} from './base'
-import {googleProvider} from './base'
 
 import './SignIn.css'
+import quill from './quill.svg'
+import googleLogo from './google.svg'
+import { auth, googleProvider } from './base'
 
 const SignIn = () => {
-    const authenticate = () => {
-        auth.signInWithPopup(googleProvider)
-    }
+  const authenticate = () => {
+    auth.signInWithPopup(googleProvider)
+  }
 
-    return (
-        <div className = "SignIn">
-            <button onClick = {authenticate}>SignIn</button>
-        </div>
-    )
+  return (
+    <div className="SignIn">
+      <header className="Header">
+        <img src={quill} alt="" />
+        <span className="title">Noteherder</span>
+      </header>
+      <main>
+        <h3>Hey, Nerd! You Like Notes?</h3>
+        <p>You never know when you'll need to write crap down. In fact, you should probably be taking notes right now.</p>
+
+        <button
+          className="google"
+          onClick={authenticate}
+        >
+          <img src={googleLogo} alt="" />
+          Sign in with Google
+        </button>
+      </main>
+    </div>
+  )
 }
 
 export default SignIn
